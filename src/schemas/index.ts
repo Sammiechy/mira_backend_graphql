6ed4f839d7fc03a167290d59164ff4c7c8a3ba7f
shammi <amrita.choudhary41@gmail.com>
2024-12-29 22:56:13 +0530

    changes in the schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,10 +2,11 @@ import { buildSchema } from "type-graphql";
 import { UserResolver } from "../resolvers/UserResolver";
 import { OrganizationResolver } from "../resolvers/OrganizationResolver";
 import { ShipperResolver } from "../resolvers/ShipperResolver";
+import { RecieverResolver } from "../resolvers/RecieverResolver";
 
 export const createSchema = () => {
   return buildSchema({
-    resolvers: [UserResolver, OrganizationResolver,ShipperResolver],
+    resolvers: [UserResolver, OrganizationResolver,ShipperResolver, RecieverResolver],
     validate: false,
   });
 };
